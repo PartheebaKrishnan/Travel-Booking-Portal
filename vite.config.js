735d385f29898ca47d5498f53fdc6d6123215e16
PartheebaKrishnan <partheebakrishnan1012@gmail.com>
2026-05-06 12:25:56 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/travel-app/',
   plugins: [react()],
   server: {
     proxy: {
